3cbb7191338cf05e39103e62aa74b3b45e449275
yyoungs <18051415+yyoungs@users.noreply.github.com>
2021-06-26 17:23:44 +0100

    move enter and caps

--- keyboards/crkbd/keymaps/yyoungs/keymap.c
+++ keyboards/crkbd/keymaps/yyoungs/keymap.c
@@ -10,13 +10,13 @@ extern uint8_t is_master;
 
 //Tap Dance Declarations
 enum {
-  TD_LSFT_CAPS = 0
+  TD_LALT_CAPS = 0
 };
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for L Shift, twice for Caps Lock
-  [TD_LSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
+  [TD_LALT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_CAPS)
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -49,18 +49,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                              |--------+--------+--------+--------+--------+----------|
             KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                   KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  \
         //|--------+--------+--------+--------+--------+--------|                              |--------+--------+--------+--------+--------+----------|
-  TD(TD_LSFT_CAPS),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                   KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT,\
+            KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                   KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT,\
         //|--------+--------+--------+--------+--------+--------+---------------|     |--------+--------+--------+--------+--------+--------+----------|
-                                                KC_LALT,  RAISE, LCTL_T(KC_SPC),        KC_SPC,  LOWER,  KC_LGUI \
-                                            //`---------------------------------'     `--------------------------'
+                             TD(TD_LALT_CAPS),  MT(RAISE, KC_SPC), LCTL_T(KC_ENT),        KC_SPC,  LOWER,  KC_LGUI \
+                                            //`-------------------------------'     `--------------------------'
 
   ),
 
   [_LOWER] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,\
+       KC_F11,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_F6, KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F12,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_PSCR, KC_PGUP,  KC_PGDN, KC_HOME, KC_END,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_NO,  KC_NO,\
+      KC_TRNS, KC_PSCR, KC_PGUP,  KC_PGDN, KC_HOME, KC_END,                      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_NO, KC_F11,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TRNS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
