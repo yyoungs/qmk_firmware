0e0770407e6d422793d2d2410d0792203dd5c89c
yyoungs <18051415+yyoungs@users.noreply.github.com>
2021-07-03 14:54:29 +0100

    revert caps tap

--- keyboards/crkbd/keymaps/yyoungs/keymap.c
+++ keyboards/crkbd/keymaps/yyoungs/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //|--------+--------+--------+--------+--------+--------|                              |--------+--------+--------+--------+--------+----------|
             KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                   KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_SFTENT,\
         //|--------+--------+--------+--------+--------+--------+---------------|     |--------+--------+--------+--------+--------+--------+----------|
-                             TD(TD_LALT_CAPS),  MT(RAISE, KC_SPC), LCTL_T(KC_ENT),        KC_SPC,  LOWER,  KC_LGUI \
+                                        TD(TD_LALT_CAPS),  RAISE, LCTL_T(KC_SPC),        KC_SPC,  LOWER,  KC_LGUI \
                                             //`-------------------------------'     `--------------------------'
 
   ),
